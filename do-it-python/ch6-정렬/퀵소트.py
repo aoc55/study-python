e9b8ce0ffa3d51b8b377c26805fb2bfb11441de3
aoc55 <aoc55.soft@gmail.com>
2023-07-18 22:49:41 +0900

    0718

--- do-it-python/ch6-정렬/퀵소트.py
+++ do-it-python/ch6-정렬/퀵소트.py
@@ -2,10 +2,10 @@
     퀵소트
 """
 
-
 from typing import MutableSequence
 
 
+# Partion 함수 실습
 def partition(a: MutableSequence) -> None:
     n = len(a)
     pl = 0
@@ -37,7 +37,7 @@ def partition(a: MutableSequence) -> None:
     print(*a[pr+1:n])
 
 
-
+# 퀵소트 - 재귀 버전
 def qsort(a: MutableSequence, left: int, right:int) -> None:
     pl = left
     pr = right
@@ -54,6 +54,7 @@ def qsort(a: MutableSequence, left: int, right:int) -> None:
             pl += 1
             pr -= 1
 
+    # 재귀를 통한 divde & conquer
     if left < pr:
         qsort(a, left, pr)
     if right > pl:
@@ -64,12 +65,53 @@ def quick_sort(a: MutableSequence) -> None:
     qsort(a, 0, len(a)-1)
 
 
+# 퀵소트 - 비재귀버전
+def qsort_non_recursive(a: MutableSequence, left: int, right: int) -> None:
+
+    # Stack
+    stack = list()
+    stack.append((left, right))
+
+    while len(stack) > 0:
+        left, right = stack.pop()
+        print(f'Stack 에서 꺼냄 => left:{left} / right:{right}')
+        pl, pr = left, right
+
+        # Pivot
+        x = a[(left + right) // 2]
+
+        # 정렬
+        while pl <= pr:
+            while a[pl] < x:
+                pl += 1
+            while a[pr] > x:
+                pr -= 1
+
+            if pl <= pr:
+                print(f'idx:{pl} <-> idx:{pr} Swap!!')
+                a[pl], a[pr] = a[pr], a[pl]  # Swap
+                pl += 1
+                pr -= 1
+
+        if left < pr:
+            stack.append((left, pr))
+
+        if pl < right:
+            stack.append((pl, right))
+
+
+def quick_sort_non_recursive(a: MutableSequence) -> None:
+    qsort_non_recursive(a, 0, len(a)-1)
+
 
 if __name__ == '__main__':
     # arr = [1, 8, 7, 4, 5, 2, 6, 3, 9]
     # arr = [9, 8, 7, 5, 5, 12, 2, 10]
     arr = [3, 10, 9, 4, 15, 5]
-    partition(arr)
+    #
+    # partition(arr)
     # quick_sort(arr)
+    # print(arr)
+    quick_sort_non_recursive(arr)
     print(arr)
 
